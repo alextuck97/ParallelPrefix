f8153ba0d5d67b7e6fc0d9bf942b8a1b37133107
Alex Tuck <alex.tuck@wsu.edu>
2019-10-30 16:15:02 -0700

    Parallel prefix works as intended

--- ParallelPrefix.c
+++ ParallelPrefix.c
@@ -19,12 +19,14 @@ struct Matrix2x2 ParallelPrefix(struct Matrix2x2 * M, int procs, int my_rank, in
         MPI_Status status;
         struct Matrix2x2 offset_remote;
         MPI_Recv(&offset_remote.m[0][0], 4, MPI_INT, partner, 0, MPI_COMM_WORLD, &status);
+        
+        offset = MMmultiplyMod(&offset, &offset_remote, P);
 
         if(partner < my_rank)
         {
-            offset = MMmultiplyMod(&offset_remote, &offset, P);
+            local = MMmultiplyMod(&offset_remote, &local, P);
         }
     }
 
-    return offset;
+    return local;
 }
--- ParallelTestSuite.c
+++ ParallelTestSuite.c
@@ -12,5 +12,9 @@ bool TestParallelPrefix(struct Matrix2x2 * m, int procs, int my_rank, int P)
         serial_result = MMmultiplyMod(&serial_result, m, P);
     }
 
+    printf("My rank %d serial: A=%d B=%d C=%d D=%d   parallel: A=%d B=%d C=%d D=%d\n", my_rank, serial_result.m[0][0],
+    serial_result.m[1][0], serial_result.m[0][1], serial_result.m[1][1], my_offset.m[0][0], my_offset.m[1][0], 
+    my_offset.m[0][1], my_offset.m[1][1]);
+
     return compareMatrices(&serial_result, &my_offset);
 }
