f2d35b1320baf90dcb8504f6689fa6bfc6931f7e
tuckandro11 <alex.tuck@wsu.edu>
2019-11-02 17:15:45 -0700

    Results in csv format

--- main.c
+++ main.c
@@ -93,7 +93,7 @@ int main(int argc,char *argv[])
         if(rank == 0)
         {
             float time_avg = (float)time_sum / (float)trials;
-            printf("%f ", time_avg);
+            printf("%f,", time_avg);
         }
         
     }
